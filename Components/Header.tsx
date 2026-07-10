8adb3a1e3ea64e1182bace0c9bbd4358d7a1c4b2
mabdullah356 <abdullahworld111@gmail.com>
2026-07-11 00:03:44 +0500

    feat: initialize project structure with header component and tailwind styles

---
+++ Components/Header.tsx
@@ -0,0 +1,36 @@
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/api-svg.svg";
+
+
+export default function Header() {
+    return (
+        <nav className="w-full py-4 px-8 h-16 bg-gray-50 shadow-md border-b-1 border-gray-200">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-2xl font-bold flex gap-2 items-center">
+                    <div className="relative h-10 w-10 overflow-hidden">
+                        <Image src={logo} alt="Logo" fill className="object-cover" />
+                    </div>
+                    <Link href="/">
+                        <h1 className="hover:text-[#f56565] transition-colors">Random API</h1>
+                    </Link>
+                </div>
+                <div className="flex gap-8">
+                    <Link href="/" className="hover:text-[#f56565] transition-colors font-medium">
+                        Home
+                    </Link>
+                    <Link href="/" className="hover:text-[#f56565] transition-colors font-medium">
+                        API
+                    </Link>
+                    <Link href="/" className="hover:text-[#f56565] transition-colors font-medium">
+                        Docs
+                    </Link>
+                    <Link href="https://github.com/mabdullah356/randomApi" target="_blank" className="flex hover:text-[#f56565] transition-colors text-xl items-center">
+                        <FaGithub />
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+}
